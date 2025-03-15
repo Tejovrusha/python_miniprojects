7ffa58fa47a4079570fdef462fb06e6a8a38e90b
Tejovrusha <tejovrusha416@gmail.com>
2025-03-15 06:16:31 +0000

    updated madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -20,13 +20,34 @@ adj5=input("Enter another adjective (describing word): ")
 verb=input("Enter a verb (action word): ")
 
 
+<<<<<<< HEAD
+print("Title: The Quest for the Lost Relic")
+print(f"\nIn the heart of the {place}, a brave {creature} named {name} set out on a quest. 
+Their mission? To retrieve the legendary {obj}, which had been stolen by a {adj1} sorcerer.")
+=======
 print("\nTitle: The Quest for the Lost Relic")
 print(f"\nIn the heart of the {place}, a brave {creature} named {name} set out on a quest. \nTheir mission? To retrieve the legendary {obj}, which had been stolen by a {adj1} sorcerer.")
+>>>>>>> 43efcc82eca23b9d9dc246035ae09f1813d9c2c5
 
+<<<<<<< HEAD
+print(f"\nWith a {adj2} sword in hand and a {animal} by their side, they ventured through {adj3} forests and across {adj4} mountains. 
+Along the way, they met a {mysticalbeing} who offered them a {usefulobject} to help on their journey.")
+=======
 print(f"\nWith a {adj2} sword in hand and a {animal} by their side, they ventured through {adj3} forests and across {adj4} mountains. \nAlong the way, they met a {mysticalbeing} who offered them a {usefulobject} to help on their journey.")
+>>>>>>> 43efcc82eca23b9d9dc246035ae09f1813d9c2c5
 
+<<<<<<< HEAD
+print(f"\nFinally, after a {number}-day journey, they reached the sorcerer’s {darkplace}. 
+The air was thick with {adj5} magic, and shadows danced on the walls.")
+=======
 print(f"\nFinally, after a {number}-day journey, they reached the sorcerer’s {darkplace}. \nThe air was thick with {adj5} magic, and shadows danced on the walls.")
+>>>>>>> 43efcc82eca23b9d9dc246035ae09f1813d9c2c5
 
+<<<<<<< HEAD
+print(f"\nWith a mighty {verb}, {name} faced the sorcerer. A battle of {element} and spells erupted, shaking the ground. 
+But in the end, our warrior emerged victorious, holding the {obj} once more.")
+=======
 print(f"\nWith a mighty {verb}, {name} faced the sorcerer. A battle of {element} and spells erupted, shaking the ground. \nBut in the end, our warrior emerged victorious, holding the {obj} once more.")
+>>>>>>> 43efcc82eca23b9d9dc246035ae09f1813d9c2c5
 
 print(f"\nThe kingdom of {kingdom} was saved, and the people cheered, forever grateful for the courage of {name}!")
