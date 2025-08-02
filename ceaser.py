a3831dcda3b7edf9e0f4060f2f8ed30d4f5a9e7d
Tejovrusha <tejovrusha416@gmail.com>
2025-08-02 09:49:23 +0530

    Update ceaser.py

--- ceaser.py
+++ ceaser.py
@@ -1,8 +1,12 @@
+#This is a ceaser cipher with a twist. I made it such that it also shifts the numbers and puntuations which is not present in the original Ceaser Cipher logic.
+
 import string
 
-abc=string.ascii_letters
-digit=string.digits
+abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+Abc=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+digit=list(string.digits)
 punc=" "+string.punctuation
+punc=list(punc)
 
 pt=input("Enter a message: ")
 
@@ -15,18 +19,26 @@ while True:
 
 ct=""
 for i,c in enumerate(pt):
-  if c.isdigit():
+  if c in digit:
     j=digit.index(c)
-    ct[i]=digit[j+k]
+    ind=(j+k)%10
+    ct+=digit[ind]
   elif c in punc:
-    ct[i]=c
-  elif c.isalpha():
+    j=punc.index(c)
+    ind=(j+k)%33
+    ct+=punc[ind]
+  elif c in abc:
     j=abc.index(c)
-    ct[i]=abc[j+k]
+    ind=(j+k)%26
+    ct+=abc[ind]
+  elif c in Abc:
+    j=Abc.index(c)
+    ind=(j+k)%26
+    ct+=Abc[ind]
   else:
-    ct[i]=c
+    ct+=c
 
-print("Ceaser Cipher")
+print("\nCeaser Cipher")
 print(f"Shifted by {k} keys")
 print("Plain Text:",pt)
 print("Cipher Text:",ct)
